bb033e9b1ec135071bc1f8f505915f0b93bbc553
Tolu Olubode <toluwanimiolubode@gmail.com>
2018-10-12 19:51:45 -0400

    Fixed Hero text

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,11 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <div className="Hero">
-      <div className="HeroGroup">
-        <h1>Hi! I'm Tolu, a digital product designer with a strong background in code.</h1>
-        <Link to="/page-2/">Go to page 2</Link>
+      <div className="HeroText">
+        <div>
+          <iframe width="200" height="200" src="https://lottiefiles.com/iframe/43-emoji-wink" frameBorder="0" allowFullScreen scrolling='no'></iframe>
+        </div>
+        <h1>Hello there! <br></br>I ❤️ code just as much as I love pixels</h1>
       </div>
     </div>
   </div>
