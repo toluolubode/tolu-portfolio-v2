8885a3d85ff432f2ad4677f74699748c5ba26dcb
Tolu Olubode <toluwanimiolubode@gmail.com>
2018-10-13 02:31:47 -0400

    footer needs to be committed for build to pass *facepalm*

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import './Footer.css'
+
+const Footer = ({ siteTitle }) => (
+  <footer className="Footer">
+    <p>COPYRIGHT 2018 TOLU OLUBODE. ALL RIGHTS RESERVED.</p>
+  </footer>
+)
+
+export default Footer
