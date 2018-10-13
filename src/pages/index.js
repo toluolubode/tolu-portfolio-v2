32c5cd3c61eef903e7b4fe0cc9a0b58079481575
Tolu Olubode <toluwanimiolubode@gmail.com>
2018-10-12 20:28:37 -0400

    Changed Hero text

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroText">
-        <div>
+        <div className="HeroAnimate">
           <iframe width="200" height="200" src="https://lottiefiles.com/iframe/43-emoji-wink" frameBorder="0" allowFullScreen scrolling='no'></iframe>
         </div>
-        <h1>Hello there! <br></br>I ❤️ code just as much as I love pixels</h1>
+        <h1>Hello there! <br></br>I <span>❤️</span>code just as much as I ❤️ pixels</h1>
+        <p>To get you started, here's what I've done recently</p>
       </div>
     </div>
+    <div className="Case"></div>
   </div>
 )
 
