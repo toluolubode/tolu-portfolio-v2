558bbe0a4df0dd83579ed8ab25b8b1261aa9a17e
Tolu Olubode <toluwanimiolubode@gmail.com>
2018-10-13 21:52:47 -0400

    some more links

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,17 @@ const IndexPage = () => (
         </div>
         <div className="HeroWords">
           <h1>Hello there! <br></br>I <span>❤️</span>code just as much as I ❤️ pixels</h1>
-          <span id="pointer">🚀</span><p>My site is currently under construction. Here's what I've worked on recently <a href="https://uxplanet.org/designing-spotify-jamroom-f9f4ec9fd1a3">recently</a></p>
+          <span id="pointer">🚀</span><p style={{fontWeight:"bold"}}>My site is currently under construction.</p>
+          <br></br>
+          <p>Here's my most recent case study: <a style={{color:"#00bd6e"}} target="_blank" href="https://uxplanet.org/designing-spotify-jamroom-f9f4ec9fd1a3">Spotify Jamroom</a></p>
+          <br></br>
+          <p>You can catch my shenanigans on <a  target="_blank" href="https://twitter.com/txluu">Twitter</a></p>
+          <br></br>
+          <p>I sling some code on <a style={{color:"black"}} target="_blank" href="https://twitter.com/txluu">Github</a></p>
+          <br></br>
+          <p>...and</p>
+          <br></br>
+          <p>I'm super professional on <a style={{color:"darkslategray"}} target="_blank" href="https://www.linkedin.com/in/toluolubode/">Linkedin</a></p>
         </div>
       </div>
     </div>
